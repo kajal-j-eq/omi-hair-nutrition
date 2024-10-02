5d73fff5c06756a4b93c34ebdd0e901754f43662
Kajal Jain <kajal.jain@equestsolutions.net>
2024-10-02 09:56:35 +0530

    UI changes 1

--- js/custom.js
+++ js/custom.js
@@ -1,48 +1,46 @@
 $(document).ready(function () {
 
-// section-2
-function section2(){
-    // $('.announcement-bar__row__col').slick({
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     autoplaySpeed: 2000,
-    //   });
-}
+    // S-2 uspa-slider
+    $('.uspaSlider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
 
-    // product slider
-    function hairNutrition() {
-        $('#slider1').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '#slider2'
-        })
-        $('#slider2').slick({
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            asNavFor: '#slider1',
-            centerMode: true,
-            focusOnSelect: true,
-            arrows: true,
-            // swipeToSlide: true
-        })
-    }
+    // S-3 product slider
+    $('.product-slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.product-slider-nav'
+    });
+    
+    $('.product-slider-nav').slick({
+        prevArrow: $('.product-prev'),
+        nextArrow: $('.product-next'),
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.product-slider-for',
+        dots: true,
+        arrows: true,
+        focusOnSelect: true
+    });
 
-    function clinicallyProvenRes(){
-        $('.clinicallyProven-slider-container').slick({
-            prevArrow: $('.clinicallyRes-prev'),
-            nextArrow: $('.clinicallyRes-next'),
-            slidesToShow: 1, 
-            slidesToScroll: 1, 
-            dots:true,
-            autoplaySpeed: 3000
-          });
-    }
-
-      
-    // Subscription
+    // S-3 Subscription
     let lastChecked = null;
     const SubscriptionBoxDisplay = document.querySelector('.SubscriptionBoxDisplay');
     const purchaseRadio = document.querySelector('#purchaseRadio');
@@ -71,8 +69,6 @@ function section2(){
             SubscriptionBoxDisplay.style.display = 'block';
         }
     });
-
-
     mostPopular_plan.addEventListener('click', function () {
         popularPlan_title.style.backgroundColor = '#F4B4A4';
         popularPlan_border.style.border = '3px solid #F4B4A4';
@@ -82,7 +78,7 @@ function section2(){
             bestVale_title.style.backgroundColor = '#2E1A4A';
             bestVale_border.style.border = '3px solid #2E1A4A';
         }
-    })
+    });
     bestValue_plan.addEventListener('click', function () {
         if (popularPlan_title.style.backgroundColor === 'rgb(244, 180, 164)') {
             popularPlan_title.style.backgroundColor = '#2E1A4A';
@@ -90,9 +86,8 @@ function section2(){
             bestVale_title.style.backgroundColor = '#F4B4A4';
             bestVale_border.style.border = '3px solid #F4B4A4';
         }
-    })
-
-    // accordion
+    });
+    // S-3 accordion
     $('.accordion-list > li > .answer').hide();
     $('.accordion-list > li').click(function () {
         if ($(this).hasClass("active")) {
@@ -104,7 +99,15 @@ function section2(){
         }
         return false;
     });
-    section2();
-    hairNutrition();
-    clinicallyProvenRes();
+
+    // S-7 Clinically Proven Res
+        $('.clinicallyProven-slider-container').slick({
+            prevArrow: $('.clinicallyRes-prev'),
+            nextArrow: $('.clinicallyRes-next'),
+            slidesToShow: 1, 
+            slidesToScroll: 1, 
+            dots:true,
+            autoplaySpeed: 3000
+          });
+
 });
