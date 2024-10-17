3b9a4d1eca20ab960e35e33590d762bb1895b3b5
Kajal Jain <kajal.jain@equestsolutions.net>
2024-10-17 18:04:16 +0530

    ui changes 3

--- js/custom.js
+++ js/custom.js
@@ -46,52 +46,62 @@ $(document).ready(function () {
         });
     }
     // S-3 Subscription
-    let lastChecked = null;
-    const SubscriptionBoxDisplay = document.querySelector('.SubscriptionBoxDisplay');
-    const purchaseRadio = document.querySelector('#purchaseRadio');
-    const subscribeRadio = document.querySelector('#subscribeRadio');
     const mostPopular_plan = document.querySelector('.mostPopular_plan');
     const bestValue_plan = document.querySelector('.bestValue_plan');
     const popularPlan_title = document.querySelector('.popularPlan_title');
     const popularPlan_border = document.querySelector('.popularPlan_border');
     const bestVale_title = document.querySelector('.bestVale_title');
     const bestVale_border = document.querySelector('.bestVale_border');
-    purchaseRadio.addEventListener('click', function (e) {
-        subscribeRadio.checked = true;
-        if (this === lastChecked) {
-            this.checked = false;
-            subscribeRadio.checked = false;
-            lastChecked = null;
-            popularPlan_title.style.backgroundColor = '#2E1A4A';
-            popularPlan_border.style.border = '3px solid #2E1A4A';
-            bestVale_title.style.backgroundColor = '#2E1A4A';
-            bestVale_border.style.border = '3px solid #2E1A4A';
-            SubscriptionBoxDisplay.style.display = 'none';
-        } else {
-            lastChecked = this;
-            popularPlan_title.style.backgroundColor = '#F4B4A4';
-            popularPlan_border.style.border = '3px solid #F4B4A4';
-            SubscriptionBoxDisplay.style.display = 'block';
-        }
+    const subscribe_radio = document.querySelector('#subscribeID');
+    const purchase_radio = document.querySelector('#purchaseID');
+    const hideData = document.querySelectorAll('.hideDetails');
+    if (subscribe_radio.checked) {
+        popularPlan_title.style.backgroundColor = '#F4B4A4';
+        popularPlan_border.style.border = '3px solid #F4B4A4';
+    }
+
+    subscribe_radio?.addEventListener('click', function () {
+        hideData?.forEach(data => {
+            data.style.display = "flex"
+        })
+    })
+    purchase_radio?.addEventListener('click', function () {
+        hideData?.forEach(data => {
+            data.style.display = "none"
+        })
     });
+
     mostPopular_plan.addEventListener('click', function () {
+        let option = document.getElementById('option1');
+        option.checked = true;
         popularPlan_title.style.backgroundColor = '#F4B4A4';
         popularPlan_border.style.border = '3px solid #F4B4A4';
         if (popularPlan_title.style.backgroundColor === 'rgb(244, 180, 164)') {
-            popularPlan_title.style.backgroundColor = '#F4B4A4';
-            popularPlan_border.style.border = '3px solid #F4B4A4';
             bestVale_title.style.backgroundColor = '#2E1A4A';
             bestVale_border.style.border = '3px solid #2E1A4A';
         }
     });
     bestValue_plan.addEventListener('click', function () {
-        if (popularPlan_title.style.backgroundColor === 'rgb(244, 180, 164)') {
+        bestVale_title.style.backgroundColor = '#F4B4A4';
+        let option = document.getElementById('option2');
+        option.checked = true;
+        bestVale_border.style.border = '3px solid #F4B4A4';
+        if (bestVale_title.style.backgroundColor === 'rgb(244, 180, 164)') {
             popularPlan_title.style.backgroundColor = '#2E1A4A';
             popularPlan_border.style.border = '3px solid #2E1A4A';
-            bestVale_title.style.backgroundColor = '#F4B4A4';
-            bestVale_border.style.border = '3px solid #F4B4A4';
         }
     });
+    let productForm = document.querySelector('.dummyCheck');
+    productForm.addEventListener('click', (e) => {
+        e.preventDefault();
+        let selectedradio = document.querySelector('input[name="productPurchase"]:checked');
+        if (selectedradio) {
+            if (selectedradio.value === 'subscribe') {
+                let option = document.querySelector('input[name="options"]:checked');
+            }
+        }
+    })
+
     // S-3 accordion
     $('.accordion-list > li > .answer').hide();
     $('.accordion-list > li').click(function () {
@@ -116,14 +126,108 @@ $(document).ready(function () {
             autoplaySpeed: 3000
         });
     }
-    if ($('.js-RH-Slider-container')?.length > 0) {
-        $('.js-RH-Slider-container').slick({
-            prevArrow: $('.regrowHair-slider-prev'),
-            nextArrow: $('.regrowHair-slider-next'),
-            slidesToShow: 1,
+    // S-10 scientifically proven
+    if ($('.js-supplement-slider')?.length > 0) {
+        $('.js-supplement-slider').slick({
+            infinite: true,
+            autoplay: true,
+            speed: 1000,
+            slidesToShow: 3,
             slidesToScroll: 1,
-            dots: true,
-            autoplaySpeed: 3000
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+            ]
         });
     }
+    // js-uspaBottom-slider
+    if ($('.js-uspaBottom-slider')?.length > 0) {
+        $('.js-uspaBottom-slider').slick({
+            infinite: true,
+            autoplay: true,
+            speed: 500,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    centerMode: true,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+            ]
+        });
+    }
+
+    $('.matchHeight-regrowTitle').matchHeight();
+    $('.matchHeight-regrowP').matchHeight();
+    // js-regrow hair-slider
+    if ($('.js-regrowingHair__slider-container')?.length > 0) {
+        const $regrowSlider = $('.js-regrowingHair__slider-container').slick({
+            prevArrow: $('.regrowHair-slider-prev'),
+            nextArrow: $('.regrowHair-slider-next'),
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            autoplaySpeed: 3000,
+            responsive: [
+                {
+                    breakpoint: 1300,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                },
+            ]
+        });
+    }
+
+    const faqHeader = document.querySelectorAll(".js-faq-acc-header");
+    faqHeader.forEach(header => {
+        header.addEventListener("click", () => {
+            const currentlyActiveHeader = document.querySelector(".js-faq-acc-header.active");
+            if (currentlyActiveHeader && currentlyActiveHeader !== header) {
+                currentlyActiveHeader.classList.remove("active");
+                currentlyActiveHeader.nextElementSibling.style.maxHeight = 0;
+            }
+
+            header.classList.toggle("active");
+            const faqBody = header.nextElementSibling;
+            if (header.classList.contains("active")) {
+                faqBody.style.maxHeight = faqBody.scrollHeight + "px";
+            } else {
+                faqBody.style.maxHeight = 0;
+            }
+        });
+    });
+
+    // video section
+    let regrowHairVideoPoster = document.querySelectorAll('.js-video-poster');
+    let regrowHairVideo = document.querySelectorAll('.js-video');
+    let imgSections = document.querySelectorAll('.imgSection');
+    imgSections?.forEach((item, index) => {
+        let currentPoster = regrowHairVideoPoster[index];
+        let currentVideo = regrowHairVideo[index];
+        item?.addEventListener('click', function () {
+            currentPoster.style.display === 'none';
+            currentVideo.play();
+            currentVideo.style.display = 'block';
+        });
+        currentVideo.addEventListener('ended', () => {
+            currentPoster.style.display = 'block';
+            currentVideo.style.display = 'none';
+        })
+    });
+
+
 });
